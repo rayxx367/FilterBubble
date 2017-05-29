c964f8d76a9908b4d3c72cc28e55ce4b568b4497
rayxx367 <rayxx367@umn.edu>
2017-05-29 13:29:15 -0500

    Always test locally!

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var serv = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.sendFile('landingpage.html')
+	res.sendFile(__dirname + '/landingpage.html')
 })
+
+var portX = process.env.PORT || 2000;
+serv.listen(portX);
